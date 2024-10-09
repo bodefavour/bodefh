2a432f5142e2d95cd201735ec9867128a5c46729
Bode Favour <bodefavour@gmail.com>
2024-10-09 09:09:59 -0700

    edit App.tsx to include all dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Hero from './components/Hero';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Skills from './components/Skills';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hero />
+      <About />
+      <Portfolio />
+      <Skills />
+      <Contact />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
