518106282cc09a5599af73d7c5d3d72a76f61343
bodefavour <bodefavour@gmail.com>
2024-10-09 18:56:21 +0100

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero: React.FC = () => {
         {/* Profile Picture */}
         <div className="relative">
           <img
-            src="/assets/images/IMG_3980_jpg@824833243.jpg"
+            src="/assets/images/IMG_3980_jpg@824833243.JPG"
             alt="Favour's Profile"
             className="w-48 h-48 rounded-full border-4 border-white object-cover"
           />
