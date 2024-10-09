0baf5f01aee570f7a65d4ff30669eb554e963da8
bodefavour <bodefavour@gmail.com>
2024-10-09 19:22:58 +0100

    Update Footer.tsx to display correct links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,9 +6,9 @@ const Footer: React.FC = () => {
       <div className="container mx-auto px-6 lg:px-20 text-center text-white">
         <p className="mb-4">&copy; {new Date().getFullYear()} Favour Bode. All rights reserved.</p>
         <div className="flex justify-center space-x-6">
-          <a href="https://linkedin.com/in/favourbode" className="hover:text-blue-400">LinkedIn</a>
-          <a href="https://github.com/favourbode" className="hover:text-gray-400">GitHub</a>
-          <a href="https://twitter.com/favourbode" className="hover:text-blue-500">Twitter</a>
+          <a href="https://linkedin.com/in/favour-bode-" className="hover:text-blue-400">LinkedIn</a>
+          <a href="https://github.com/bodefavour" className="hover:text-gray-400">GitHub</a>
+          <a href="https://x.com/favour_bode?t=Q8Dmemp7ftwWZujtuM7FsQ&s=09" className="hover:text-blue-500">Twitter</a>
         </div>
       </div>
     </footer>
