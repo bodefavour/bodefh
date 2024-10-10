2d6fdc23bd1c030fac660903f6d7b28742c1dbeb
bodefavour <bodefavour@gmail.com>
2024-10-10 15:39:02 +0100

    Update tailwind.config.js to allow for animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,3 +8,18 @@ module.exports = {
 		    },
 	  plugins: [],
 }
+module.exports = {
+  theme: {
+    extend: {
+      animation: {
+        fadeIn: 'fadeIn 2s ease-in-out',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+      },
+    },
+  },
+};
