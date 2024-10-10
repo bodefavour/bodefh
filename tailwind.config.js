156fa8d264b8ba3dd6894f1d8b64ba368a8655d8
bodefavour <bodefavour@gmail.com>
2024-10-10 15:47:51 +0100

    Update tailwind.config.js to implement animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	  content: [
-		      "./src/**/*.{js,jsx,ts,tsx}",  // Allows Tailwind to scan all React component files
-		    ],
-	  theme: {
-		      extend: {},
-		    },
-	  plugins: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",  // Allows Tailwind to scan all React component files
+  ],
+  theme: {
+    extend: {
+      animation: {
+        fadeIn: 'fadeIn 2s ease-in-out',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+      },
+    },
+  },
+  plugins: [],
 }
