fe04f1cfee23b9cc19048a389ba9f84f839a233c
bodefavour <bodefavour@gmail.com>
2024-10-10 16:21:01 +0100

    Update About.tsx to update about details

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,10 +6,10 @@ const About: React.FC = () => {
       <div className="container mx-auto px-6 lg:px-20">
         <h2 className="text-4xl font-bold text-center mb-6">About Me</h2>
         <p className="text-lg text-gray-700 text-center max-w-3xl mx-auto">
-          I'm Favour Bode, a software engineer specializing in building
-          exceptional digital experiences. With a passion for both design and
-          development, I aim to create websites and applications that are
-          visually stunning and highly performant.
+          I'm Favour Bode, a multifaceted Software Engineer with hands-on experience in cloud development, full-stack web, and mobile app development. I specialize in building scalable, high-performance applications using cutting-edge technologies like Flutter, Dart, and React, and I am exploring the potential of decentralized apps with Hedera Hashgraph. With a strong background in digital marketing and social media management, I understand how to blend technology with strategic communication to drive results.
+        </p>
+        <p className="text-lg text-gray-700 text-center max-w-3xl mx-auto">
+          I am passionate about creating user-focused solutions that solve real-world problems while pushing the boundaries of innovation. If you’re looking for a professional who can not only develop and design robust applications but also understand how to effectively market and grow your brand, let's connect! I’m always eager to collaborate on exciting projects and help businesses scale through smart technology and effective marketing strategies.
         </p>
       </div>
     </section>
