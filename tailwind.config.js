434dda0a1ec985e11a54454f486deafc8b764e40
bodefavour <bodefavour@gmail.com>
2024-10-10 15:41:17 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,18 +8,3 @@ module.exports = {
 		    },
 	  plugins: [],
 }
-module.exports = {
-  theme: {
-    extend: {
-      animation: {
-        fadeIn: 'fadeIn 2s ease-in-out',
-      },
-      keyframes: {
-        fadeIn: {
-          '0%': { opacity: 0 },
-          '100%': { opacity: 1 },
-        },
-      },
-    },
-  },
-};
