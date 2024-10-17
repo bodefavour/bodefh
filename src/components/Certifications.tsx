7b092244dd0b99ebf43ee4ed87364857e271c7af
bodefavour <bodefavour@gmail.com>
2024-10-17 10:09:00 +0100

    Update Certifications.tsx to correct image alignment

--- src/components/Certifications.tsx
+++ src/components/Certifications.tsx
@@ -87,7 +87,7 @@ const Certifications = () => {
     objectFit: 'cover',
     borderRadius: '0',
     marginBottom: '15px',
-    box-shadow: '0 4px 8px rgba(0, 0, 0, 0.2)', /* Optional: Adds shadow for better visuals */
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)', /* Optional: Adds shadow for better visuals */
   };
 
   return (
